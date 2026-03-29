b4e33a56929de0756bece5cd1fa46e5ec00e3d7c
Vincent <michaelv.pantaleon@gmail.com>
2026-03-29 22:27:03 +0800

    Added Comments

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ import { UpdateTask } from './operations/update.js';
 
 /* 
     Sample Usage: 
+        # Adding a new task
+        task-cli add "Buy groceries"
+        # Output: Task added successfully (ID: 1)
+
         # Updating and deleting tasks
         task-cli update 1 "Buy groceries and cook dinner"
         task-cli delete 1
@@ -26,7 +30,7 @@ import { UpdateTask } from './operations/update.js';
 
         # Listing all tasks
         task-cli list
-        
+
         # Listing tasks by status
         task-cli list done
         task-cli list todo
