2326895fdf0afca4342e5a2ae78358fcca3edfe7
Vincent <michaelv.pantaleon@gmail.com>
2026-03-29 22:20:42 +0800

    added comments

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import fs from 'fs'
 import { AddTask } from './operations/add.js';
 import { DeleteTask } from './operations/delete.js';
 import { UpdateTask } from './operations/update.js';
+
 /*
     The user should be able to:
         1. Add, Update, and Delete tasks
@@ -12,10 +13,28 @@ import { UpdateTask } from './operations/update.js';
         5. List all tasks that are not done
         6. List all tasks that are in progress
 */
+
+/* 
+    Sample Usage: 
+        # Updating and deleting tasks
+        task-cli update 1 "Buy groceries and cook dinner"
+        task-cli delete 1
+
+        # Marking a task as in progress or done
+        task-cli mark-in-progress 1
+        task-cli mark-done 1
+
+        # Listing all tasks
+        task-cli list
+        
+        # Listing tasks by status
+        task-cli list done
+        task-cli list todo
+        task-cli list in-progress
+*/
+
 const args = process.argv.slice(2)
-
 const operation = args[0]
-
 const id = args[1] 
 const task = args.slice([2]).join(" ")
 
